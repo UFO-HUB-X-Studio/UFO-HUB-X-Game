c1006a73c6250930f6664590296bfa0100fc6944
UFO-HUB-X-Studio <panuphongartphairin@gmail.com>
2025-09-22 19:04:42 +0700

    Create MultiMapBoot.lua

---
+++ MultiMapBoot.lua
@@ -0,0 +1,5 @@
+SUPPORTED_MAPS["1234567890"] = {
+  name = "My New Map",
+  TH = "https://raw.githubusercontent.com/....../MyNewMap-TH.lua",
+  EN = "https://raw.githubusercontent.com/....../MyNewMap-EN.lua",
+}
