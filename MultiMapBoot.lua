29b4a47d379818981de522b5e5c2485d7ba67be4
UFO-HUB-X-Studio <panuphongartphairin@gmail.com>
2025-09-22 19:13:34 +0700

    Update MultiMapBoot.lua

--- MultiMapBoot.lua
+++ MultiMapBoot.lua
@@ -1,5 +1,13 @@
-SUPPORTED_MAPS["1234567890"] = {
-  name = "My New Map",
-  TH = "https://raw.githubusercontent.com/....../MyNewMap-TH.lua",
-  EN = "https://raw.githubusercontent.com/....../MyNewMap-EN.lua",
+-- MultiMapBoot.lua
+-- คืนค่าตารางแมพที่รองรับแบบ: [placeId] = { name=..., th=..., en=... }
+
+return {
+    [105555311806207] = { -- Build a Zoo
+        name = "Build a Zoo",
+        th = "https://raw.githubusercontent.com/UFO-HUB-X-Studio/Build-a-Zoo-TH/refs/heads/main/Build%20a%20Zoo%20TH.lua",
+        en = "https://raw.githubusercontent.com/UFO-HUB-X-Studio/Build-a-Zoo-EN/refs/heads/main/Build%20a%20Zoo%20EN.lua",
+    },
+
+    -- เพิ่มแมพอื่น ๆ ได้ในรูปแบบเดียวกัน:
+    -- [PLACE_ID] = { name="ชื่อเกม", th="RAW_URL_TH", en="RAW_URL_EN" }
 }
